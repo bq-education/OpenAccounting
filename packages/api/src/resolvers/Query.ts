3a9af52ba4543a20033858fce8e7960aac0c13e7
avalero <alberto.valero@bq.com>
2020-07-16 18:42:28 +0200

    hello query

--- packages/api/src/resolvers/Query.ts
+++ packages/api/src/resolvers/Query.ts
@@ -3,11 +3,13 @@ import { MongoClient, Database, Collection } from "../deps.ts";
 
 export default {
   hello: async (parent: any, app: any, ctx: any): Promise<string> => {
-    const db: Database = ctx.db;
-    const projects: Collection<IProject> = db.collection<IProject>("projects");
-    const findAll: Array<Partial<IProject>> = await projects.find({
-      _id: { $oid: "5f10746000400d6600de6c1a" },
-    });
-    return findAll[0].name!;
+    // const db: Database = ctx.db;
+    // const projects: Collection<IProject> = db.collection<IProject>("projects");
+
+    // const findAll: Array<Partial<IProject>> = await projects.find({
+    //   _id: { $oid: "5f10746000400d6600de6c1a" },
+    // });
+    // return findAll[0].name!;
+    return "hello";
   },
 };
