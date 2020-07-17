a5ef6d9655e6ab229be53800c220770deb59608e
avalero <alberto.valero@bq.com>
2020-07-17 08:34:05 +0200

    projects schema

--- packages/api/src/schema/schema.ts
+++ packages/api/src/schema/schema.ts
@@ -1,7 +1,13 @@
 import { gql } from "https://deno.land/x/oak_graphql/mod.ts";
 
 export default gql`
-  type Query{
-    hello: String!
+  type Project {
+    _id: String!
+    name: String!
+    description: String!
   }
-`
+  type Query {
+    test: String!
+    projects: [Project!]!
+  }
+`;
