b3ac6173a740be5896fa841154570a3074c1a581
avalero <alberto.valero@bq.com>
2020-07-17 08:30:53 +0200

    get projects

--- packages/api/src/resolvers/Query.ts
+++ packages/api/src/resolvers/Query.ts
@@ -2,14 +2,16 @@ import { IProject } from "../types/types.ts";
 import { MongoClient, Database, Collection } from "../deps.ts";
 
 export default {
-  hello: async (parent: any, app: any, ctx: any): Promise<string> => {
-    // const db: Database = ctx.db;
-    // const projects: Collection<IProject> = db.collection<IProject>("projects");
-
-    // const findAll: Array<Partial<IProject>> = await projects.find({
-    //   _id: { $oid: "5f10746000400d6600de6c1a" },
-    // });
-    // return findAll[0].name!;
-    return "hello";
+  test: async (parent: any, app: any, ctx: any): Promise<string> => {
+    return "working";
+  },
+  projects: async (
+    parent: any,
+    app: any,
+    ctx: any
+  ): Promise<Array<Partial<IProject>>> => {
+    const db: Database = ctx.db;
+    const projects: Collection<IProject> = db.collection<IProject>("projects");
+    return await projects.find({});
   },
 };
