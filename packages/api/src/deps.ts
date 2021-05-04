8675dedadb96fb3dd59d896af221764fb2dbb467
Alberto Valero <alberto.valero@bq.com>
2021-05-04 16:03:16 +0200

    fresh start

--- packages/api/src/deps.ts
+++
@@ -1,9 +0,0 @@
-export { Application } from "https://deno.land/x/oak/mod.ts";
-export { config } from "https://deno.land/x/dotenv/mod.ts";
-export {
-  MongoClient,
-  Database,
-  Collection,
-  ObjectId,
-} from "https://deno.land/x/mongo@v0.9.1/mod.ts";
-export { applyGraphQL, GQLError } from "https://deno.land/x/oak_graphql/mod.ts";
